25c91956acf0c82969add81b17f91d090d8414ad
elisamartin <elisamv93@gmail.com>
2019-04-15 21:05:21 +0200

    Paths corrected, register and login working

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
 
+const authRouter = require('./routing/auth-router');
+
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use('/auth', authRouter);
 
 server.get('/', (req, res) => {
 	res.send("It's alive!");
