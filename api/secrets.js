55f21659aa3dae5fda2d92ac5714d7e2542d117a
IVB107 <ianvb107@gmail.com>
2019-04-15 17:21:15 -0500

    Added get method to user-router, fixed authorization token bug

--- api/secrets.js
+++ api/secrets.js
@@ -1,3 +1,4 @@
 module.exports = {
   jwtSecret: process.env.JWT_SECRET
+  // jwtSecret: 'This is a secret'
 }
--- routing/party-router.js
+++ routing/party-router.js
@@ -2,4 +2,14 @@ const router = require('express').Router();
 
 const party = require('../data/models/party-model');
 
+router.get('/', async (req, res) => {
+  try {
+    const parties = await party.find();
+    res.status(200).json(parties);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+})
+
 module.exports = router;
