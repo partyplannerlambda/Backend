1bca8e4e9a5a4196f28593354c23c48ae06f78d6
elisamartin <elisamv93@gmail.com>
2019-04-16 20:14:49 +0200

    Added todo models and router

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const authRouter = require('../routing/auth-router');
 const partyRouter = require('../routing/party-router');
 const shoppingRouter = require('../routing/shopping-router');
+const todoRouter = require('../routing/todo-router');
 const restricted = require('../auth/restricted-middleware.js');
 
 const server = express();
@@ -16,6 +17,7 @@ server.use(cors());
 server.use('/auth', authRouter);
 server.use('/parties', restricted, partyRouter);
 server.use('/parties/:id/shopping/', restricted, shoppingRouter);
+server.use('/parties/:id/todo/', restricted, todoRouter);
 
 server.get('/', (req, res) => {
 	res.send("It's alive!");
