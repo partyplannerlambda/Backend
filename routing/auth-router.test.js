14ae6ca13d991a65552e3ebe12bf1c70f9ef98c6
elisamartin <elisamv93@gmail.com>
2019-04-18 20:08:14 +0200

    Testing: Auth router

--- routing/auth-router.test.js
+++ routing/auth-router.test.js
@@ -4,17 +4,19 @@ const request = require('supertest');
 describe('auth-router', () => {
 	describe('POST /register endpoint', () => {
 		it('returns the right response status', () => {
-			const newUser = { username: 'test3', password: 'test' };
-			return request(authRouter)
-				.post('/auth/register')
-				.send(newUser)
-				.then((res) => {
-					expect(res.status).toBe(201);
-				})
-				.catch((err) => {
-					console.log(err);
-				});
+			const newUser = { username: 'test555', password: 'test' };
+			return request(authRouter).post('/auth/register').send(newUser).then((res) => {
+				expect(res.status).toBe(201);
+			});
+		});
+	});
+	describe('POST /login endpoint', () => {
+		it('returns status 401 when trying to login an unregister user', () => {
+			const newUser = { username: 'testy', password: 'testy' };
+
+			return request(authRouter).post('/auth/login').send(newUser).then((res) => {
+				expect(res.status).toBe(401);
+			});
 		});
 	});
 });
-// comment
