00fe61e3fc2ba2d63d0b175c686cdf575bbe6794
IVB107 <ianvb107@gmail.com>
2019-04-18 12:41:44 -0500

    Created test file for shopping router

---
+++ routing/shopping-router.test.js
@@ -0,0 +1,67 @@
+const request = require('supertest');
+
+const router = require('./shopping-router.js');
+
+describe('shopping-router.js', () => {
+
+  describe('GET /shopping', () => {
+
+    it('Should return status code 200', () => {
+      // Stuff
+    });
+
+    it('Should return an array', () => {
+      // Stuff
+    })
+
+  });
+
+  describe('GET /shopping/:id', () => {
+
+    it('Should return status code 200', () => {
+      // Stuff
+    });
+
+    it('Should return an array', () => {
+      // Stuff
+    })
+    
+  });
+
+  describe('POST /shopping', () => {
+
+    it('Should return status code 200', () => {
+      // Stuff
+    });
+
+    it('Should return an array', () => {
+      // Stuff
+    })
+    
+  });
+
+  describe('PUT /shopping/:id', () => {
+
+    it('Should return status code 200', () => {
+      // Stuff
+    });
+
+    it('Should return an array', () => {
+      // Stuff
+    })
+    
+  });
+
+  describe('DELETE /shopping/:id', () => {
+
+    it('Should return status code 200', () => {
+      // Stuff
+    });
+
+    it('Should return an array', () => {
+      // Stuff
+    })
+    
+  });
+
+});
